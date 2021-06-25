25cc196b604b80048cd0ea28c4afa8d83c1a81e1
Jill <jilltu330@gmail.com>
2021-06-25 21:00:33 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req,res)=>{
-  res.send('tiny url project')
+  res.render('index')
 })
 
 app.listen(PORT, ()=>[
