bc23c3cc22002aeaba54d945b8f85d53518ee50b
Jill <jilltu330@gmail.com>
2021-06-26 15:52:21 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { urlencoded } = require('express')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const random = require('string-random')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = 3000
 
@@ -9,6 +10,16 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb://localhost/shorten-url', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', ()=> {
+  console.log('mongodb error.')
+})
+db.once('open', ()=>{
+  console.log('mongodb connected.')
+})
+
 
 app.get('/', (req, res) => {
   res.render('index')
