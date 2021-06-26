cf90fd68f22250d8aaf74cfd70a2167a4e12760c
Jill <jilltu330@gmail.com>
2021-06-26 16:25:36 +0800

    feat: add model create & page redirect

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const random = require('string-random')
 const mongoose = require('mongoose')
+
 const app = express()
 const PORT = 3000
+
+const ShortenUrl = require('./models/shortenUrl')
+const shortenUrl = require('./models/shortenUrl')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -13,10 +17,10 @@ app.use(express.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost/shorten-url', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-db.on('error', ()=> {
+db.on('error', () => {
   console.log('mongodb error.')
 })
-db.once('open', ()=>{
+db.once('open', () => {
   console.log('mongodb connected.')
 })
 
@@ -26,13 +30,21 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
+  const url = req.body.url
   const header = req.headers.origin
   const hash = random(5)
-  const result = header+'/'+hash
-  res.render('result', { result })
+  const result = header + '/' + hash
+  return ShortenUrl.create({ hash, url })
+    .then(() => res.render('result', { result }))
+    .catch(error => { console.log(error) })
 })
 
-
+app.get('/:hash', (req,res)=>{
+  const hash = req.params.hash
+  return ShortenUrl.findOne({hash})
+    .then(shortenUrl=> res.redirect(shortenUrl.url))
+    .catch(error => { console.log(error) })
+})
 
 app.listen(PORT, () => [
   console.log(`App is running on http://localhost:${3000}`)
