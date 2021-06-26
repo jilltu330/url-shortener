efee3bffedd02620421f4e5de4a871bdf68b2df9
Jill <jilltu330@gmail.com>
2021-06-26 14:14:11 +0800

    feat: result.hbs add copy function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { urlencoded } = require('express')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
@@ -6,12 +7,16 @@ const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/', (req, res) => {
-  res.render('result')
+  const result = req.body.url
+  console.log(result)
+  res.render('result', { result })
 })
 
 app.listen(PORT, () => [
